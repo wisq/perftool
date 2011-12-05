1a340029abfaa8d45eef9ec8c99a44b9b26bbf0b
Adrian Irving-Beer <adrian@shopify.com>
2011-12-05 14:48:34 -0500

    Adjust epoch.

--- perf.rb
+++ perf.rb
@@ -4,6 +4,6 @@ $LOAD_PATH << File.dirname(__FILE__) + '/lib'
 require 'perf/bisect'
 
 Perf::Bisect.new.run(
-  '784e9017b8fe93bd1f23689509ec1dc2b25d4f6c',
+  'fdfac297cdcbeb0323542bc387c93224a432c941',
   '6606aa80ec7a81085bc062655f239699dd24f1ff'
 )
